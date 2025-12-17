d5c1e49393e866e7dfdc5e210e014e88d086e83e
Sabitoshi <undertale2006rus@gmail.com>
2025-12-17 13:27:12 +0300

    fix+

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/Palitre-Generator/' : '/',
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   }
 })
 
